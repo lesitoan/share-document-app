7239faf48b606910623bf51f8f36deb50d9e98df
lesitoan <lesitoan.media@gmail.com>
2024-04-30 15:42:40 +0700

    create header, footer p1

--- controllers/docsController.js
+++ controllers/docsController.js
@@ -2,7 +2,9 @@ const pool = require('../config/connetDB');
 
 const test = async (req, res) => {
     try {
-        res.render('test');
+        res.render('pages/homePage', {
+            title: "toandz"
+        });
     } catch (err) {
         console.log(err);
     }
